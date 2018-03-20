1f56d7987ee18037fb455e833252ab18e34386e8
Maria <Maria@192.168.1.49>
2018-03-21 00:42:46 +0100

    Repasando JavaScript 1

---
+++ public_html/codigo.js
@@ -0,0 +1,7 @@
+var mensaje = "Hola Mundo! \n Qué facil es incluir 'comillas simples' y \n \"comillas dobles\".";
+
+// Al cargarse el archivo JavaScript, se muestra un mensaje
+alert(mensaje);
+ 
+// Despues del primer mensaje, se muestra otro mensaje seguido
+alert("Soy el primer script");
